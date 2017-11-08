1697a7cba96866fbdc2660dc40b2ccc44552778c
ezwelty <ethan.welty@gmail.com>
2017-11-08 15:39:04 -0700

    Add support for versions

--- scripts/build.js
+++ scripts/build.js
@@ -8,12 +8,12 @@ set('-e');
 mkdir('-p', 'web_deploy')
 
 cp('-R', 'web/*', 'web_deploy/');
+cp('-R', 'spec/*', 'web_deploy/');
 
-exec('npm run swagger bundle --        -o web_deploy/swagger.json');
-exec('npm run swagger bundle -- --yaml -o web_deploy/swagger.yaml');
+// exec('npm run swagger bundle --        -o web_deploy/swagger.json');
+// exec('npm run swagger bundle -- --yaml -o web_deploy/swagger.yaml');
 
 var SWAGGER_UI_DIST = Path.dirname(require.resolve('swagger-ui'));
 rm('-rf', 'web_deploy/swagger-ui/')
 cp('-R', SWAGGER_UI_DIST, 'web_deploy/swagger-ui/')
-sed('-i', 'http://petstore.swagger.io/v2/swagger.json', '../swagger.json', 'web_deploy/swagger-ui/index.html')
-
+// sed('-i', 'http://petstore.swagger.io/v2/swagger.json', '../swagger.json', 'web_deploy/swagger-ui/index.html')
--- scripts/deploy-branch.js
+++ scripts/deploy-branch.js
@@ -10,7 +10,8 @@ var branch = process.env.TRAVIS_BRANCH && process.env.TRAVIS_BRANCH.toLowerCase(
 if (branch && branch !== 'gh-pages') {
   var branchPath = path.join('.tmp', 'preview', branch, '/');
   mkdir('-p', branchPath);
-  exec('npm run swagger bundle -- -o ' + branchPath + 'swagger.json');
+  cp('-R', 'spec/*', branchPath);
+  // exec('npm run swagger bundle -- -o ' + branchPath + 'swagger.json');
   cp('web/index.html', branchPath);
   exec('deploy-to-gh-pages --update .tmp');
 }
